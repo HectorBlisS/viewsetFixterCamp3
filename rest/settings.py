79a10506ecc4d943a51c8ff1e0f19c02b338c263
BlisS <contacto@fixter.org>
2017-02-18 14:57:45 -0600

    ye4

--- rest/settings.py
+++ rest/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'staticfiles'),)
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':[
