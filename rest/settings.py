40a54cc57c5c7a4a7385e4ee082dda49c6c25530
BlisS <contacto@fixter.org>
2017-02-18 14:39:17 -0600

    lo que faltaba :hankey:

--- rest/settings.py
+++ rest/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = (os.path.join(BASE_DIR,'staticfiles'))
+STATICFILES_DIRS = (os.path.join(BASE_DIR,"static"),)
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':[
